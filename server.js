11dd202decf6a0f2565efb64db63e53d7f70cbbe
Kenny Murillo <kenmury@email.unc.edu>
2023-05-04 00:30:21 -0400

    init app

--- server.js
+++ server.js
@@ -2,3 +2,11 @@ import express from 'express';
 import minimist from 'minimist';
 import {RPS, RPSLS} from './lib/rpsls.js';
 
+const args = minimist(process.argv.slice(2));
+const port = args.port || args.p || 5000;
+
+const app = express();
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
